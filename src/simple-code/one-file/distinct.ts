f29acff74fca89bbfd735c305bb75e9a3c93e42a
yuto-yuto <yuto.nakatsu@gmail.com>
2021-07-06 19:14:52 +0200

    add async work for distinct

--- src/simple-code/one-file/distinct.ts
+++ src/simple-code/one-file/distinct.ts
@@ -103,6 +103,26 @@ function test<T>(array: T[], funcs: UniqFunc<T>[], loopCount = 1) {
     }
     return times;
 }
+async function testAsync<T>(array: T[], funcs: UniqFunc<T>[], loopCount = 1): Promise<number[]> {
+    const promises: Promise<number>[] = funcs.map((func) => {
+        return new Promise((resolve) => {
+            console.log(func.name + " --- start")
+            const result = execute(array, func, loopCount);
+            console.log(func.name + " --- end")
+            resolve(result);
+        });
+    });
+    const times = await Promise.all(promises);
+    return times.map(x => Math.round(x));
+}
+
+function execute<T>(array: T[], func: UniqFunc<T>, loopCount: number): number {
+    const start = performance.now();
+    for (let i = 0; i < loopCount; i++) {
+        const result = func(array);
+    }
+    return performance.now() - start;
+}
 
 const funcsForPrimitive = [
     uniqByForOf,
@@ -121,20 +141,26 @@ const funcsForObjects = [
 ];
 test(objects, funcsForObjects);
 
-[
-    { arrayLen: 10, loopRangeS: 100000, loopRangeE: 500000, step: 100000 },
-    { arrayLen: 100, loopRangeS: 10000, loopRangeE: 500000, step: 5000 },
-    { arrayLen: 1000, loopRangeS: 10000, loopRangeE: 50000, step: 5000 },
-].forEach((settings) => {
-    console.log(`length: ${settings.arrayLen}`);
-    const array = generatePrimitiveArray(settings.arrayLen, 10);
+function measure() {
+    const settings = [
+        { arrayLen: 10, loopRangeS: 10000, loopRangeE: 200000, step: 5000 },
+        { arrayLen: 10, loopRangeS: 100000, loopRangeE: 500000, step: 50000 },
+        { arrayLen: 100, loopRangeS: 100000, loopRangeE: 500000, step: 50000 },
+        { arrayLen: 1000, loopRangeS: 100000, loopRangeE: 500000, step: 50000 },
+    ];
+    for (const setting of settings) {
+        console.log(`length: ${setting.arrayLen}`);
+        const array = generatePrimitiveArray(setting.arrayLen, 10);
 
-    const titles = funcsForPrimitive.map(x => x.name).join(",");
-    console.log(`loop count,${titles}`);
-    for (let i = settings.loopRangeS; i <= settings.loopRangeE; i += settings.step) {
-        const times = test(array, funcsForPrimitive, i);
-        const data = times.join(", ");
-        console.log(`${i}, ${data}`);
+        const titles = funcsForPrimitive.map(x => x.name).join(",");
+        console.log(`loop count,${titles}`);
+        for (let i = setting.loopRangeS; i <= setting.loopRangeE; i += setting.step) {
+            const times = test(array, funcsForPrimitive, i);
+            const data = times.join(", ");
+            console.log(`${i}, ${data}`);
+        }
+        console.log();
     }
-    console.log();
-});
+}
+
+measure();
